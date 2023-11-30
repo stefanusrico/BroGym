30d6a5f041ba5f92e9bcac28288ac0419ac2ef58
stefanusrico <stefanus.rico70@gmail.com>
2023-11-30 10:27:19 +0700

    update nama

--- resources/views/page/admin/member/datamember.blade.php
+++ resources/views/page/admin/member/datamember.blade.php
@@ -17,7 +17,7 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Data Akun</h1>
+        <h1>Data Member</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
